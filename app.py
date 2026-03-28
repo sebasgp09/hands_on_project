59cabc98b6a83f0c4d13d3b910f694f8980af189
Sebastian <sebasgp09@gmail.com>
2026-03-27 21:48:34 -0500

    Fix: Flask configurado en 0.0.0.0 para despliegue en docker

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def predict():
     return render_template('home.html', prediction_text=f'El costo estimado del seguro es: ${result}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
